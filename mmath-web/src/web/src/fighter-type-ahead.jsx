b240df98b9fd94cca5e88324614eb4942b76601d
lamarios <uxg593k4>
2017-04-09 14:05:44 +0800

    two way results, with responsive handling fighter details.

--- mmath-web/src/web/src/fighter-type-ahead.jsx
+++ mmath-web/src/web/src/fighter-type-ahead.jsx
@@ -2,31 +2,27 @@ var React = require('react');
 
 var FighterTypeAhead =
     React.createClass({
-                          render: function () {
-                              return (
-                                  <ul className="fighters">
-                                      {
+        render: function () {
+            return (
+                <ul className="fighters">
+                    {
 
-                                          this.props.fighters.map(
-                                              function (fighter) {
-                                                  var style = {
-                                                      backgroundImage: 'url('
-                                                                       + fighter.picture
-                                                                       + ')'
-                                                  };
+                        this.props.fighters.map(
+                            function (fighter) {
+                                var style = {
+                                    backgroundImage: 'url(' + fighter.picture + ')'
+                                };
 
-                                                  return <li
-                                                      onClick={this.props.onClick.bind(
-                                                          null, fighter)}
-                                                      key={fighter.id}
-                                                      data-id={fighter.id}>
-                                                      <div className="icon" style={style}></div>
-                                                      {fighter.name}</li>
-                                              }.bind(this))
+                                return <li onClick={this.props.onClick.bind(null, fighter)}
+                                           key={fighter.id}
+                                           data-id={fighter.id}>
+                                    <div className="icon" style={style}></div>
+                                    {fighter.name}</li>
+                            }.bind(this))
 
-                                      }
-                                  </ul>
-                              );
-                          }
-                      });
+                    }
+                </ul>
+            );
+        }
+    });
 module.exports = FighterTypeAhead
--- mmath-web/src/web/src/loader.jsx
+++ mmath-web/src/web/src/loader.jsx
@@ -1,14 +1,14 @@
 var React = require('react');
 
 var Loader = React.createClass({
-                                   render: function () {
-                                       return (
-                                           <div>
-                                            <div className="loader"></div>
-                                           </div>
-                                       );
-                                   }
+    render: function () {
+        return (
+            <div>
+                <div className="loader"></div>
+            </div>
+        );
+    }
 
-                               });
+});
 
 module.exports = Loader;
